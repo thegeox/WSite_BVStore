bd51efce3abcd2047ea6c895997ea960b5dce367
thegeox <thegeox@users.noreply.github.com>
2020-04-06 18:28:59 -0600

    Updates

---
+++ precache-manifest.b7d6d9abc5c597269801032182599d6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ff278d507e5489c3216a8b16e4530a",
+    "url": "/WSite_BVStore.git/index.html"
+  },
+  {
+    "revision": "36e55768c535462cb1e8",
+    "url": "/WSite_BVStore.git/static/css/main.f3d693f2.chunk.css"
+  },
+  {
+    "revision": "485c749d791dcd5e1d46",
+    "url": "/WSite_BVStore.git/static/js/2.521d8a16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WSite_BVStore.git/static/js/2.521d8a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e55768c535462cb1e8",
+    "url": "/WSite_BVStore.git/static/js/main.6aa1b6ac.chunk.js"
+  },
+  {
+    "revision": "40c0f84f0e7c3ac7b2b8",
+    "url": "/WSite_BVStore.git/static/js/runtime-main.da8bc8b9.js"
+  },
+  {
+    "revision": "d5d0a3381a9bfe1badcf5c45fd2112d2",
+    "url": "/WSite_BVStore.git/static/media/Logo.d5d0a338.png"
+  },
+  {
+    "revision": "076a92beea158b2c14c4813816164168",
+    "url": "/WSite_BVStore.git/static/media/about_2.076a92be.jpg"
+  },
+  {
+    "revision": "acb12c24e512026a6f265422a3c4e11a",
+    "url": "/WSite_BVStore.git/static/media/about_3.acb12c24.jpg"
+  },
+  {
+    "revision": "a9ecd52a5c740392e42d27cfd7c80d05",
+    "url": "/WSite_BVStore.git/static/media/productLine_1.a9ecd52a.png"
+  }
+]);
